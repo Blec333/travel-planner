0cfc375a5c09e7672bb5e3c26ad7db640f675ac6
Keerti Chaudhary <17keerti@gmail.com>
2022-05-04 12:22:04 -0700

    add get routes for travellers

--- routes/api/traveller.js
+++ routes/api/traveller.js
@@ -1,16 +1,30 @@
 const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const { Traveller, Location, Trip } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  try {
+    const travellerData = await Traveller.findAll();
+    res.json(200).json(travellerData);
+  } catch (err) {
+    res.json(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  try {
+    const travellerData = await Traveller.findByPk(req.params.id, {
+      include: [{ model: Location }, { model: Trip }],
+    });
+    if (!travellerData) {
+      res.status(404).json({ message: 'No data found with that id!' });
+      return;
+    }
+    res.status(200).json(travellerData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
